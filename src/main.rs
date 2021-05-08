31176980cc4face66982a007dc5a20ef34d1fdf5
youngtrashbag <yuule.smeker@gmail.com>
2021-05-08 12:47:18 +0200

    chore: add deps for backend
    and add some boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    match std::env::var("RUST_LOG") {
+        Err(_) => {
+            std::env::set_var("RUST_LOG", "info");
+        },
+        Ok(_) => {()}
+    };
+
+    env_logger::init();
+
+    dotenv::dotenv().ok();
+
+    let db_path = std::env::var("DB_PATH").expect("DB_PATH in environment file not set");
+    let db = sled::open(db_path).unwrap();
 }
