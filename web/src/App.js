866d7ffbc8ca5a2080b335119ac3ee67110f4e81
youngtrashbag <yuule.smeker@gmail.com>
2021-05-09 16:52:37 +0200

    feat: create form for to enter word into
    - update styling
    - install formik

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,65 @@
+import { Formik, Form, Field } from "formik";
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Word Machine</h1>
-    </div>
-  );
+    // only a single word
+    const regexPattern = /^([a-z])\w{2,20}[^ ]*/;
+
+    const initialValues = {
+        word: ""
+    };
+    
+    function validateWord(value) {
+        var error;
+
+        if (!value) {
+            error = "Required";
+        } else if (!regexPattern.test(value)) {
+            error = "Please Enter a single word between 2 and 20 characters long";
+        }
+
+        console.log(error);
+        return error;
+    }
+
+    const onSubmit = ( data ) => {
+        // returns array; take first element of array
+        const word = data.word.match(regexPattern);
+
+        console.log(word);
+    };
+
+    return (
+        <div className="App">
+            <div className="Wrapper">
+                <h1>Word Machine</h1>
+                <div className="SameLine">
+                    <Formik
+                    initialValues={initialValues}
+                    onSubmit={onSubmit}
+                    >
+                    { ( formik ) => {
+                        return (
+                        <Form>
+                            <Field
+                                id="word"
+                                name="word"
+                                value={formik.values.word}
+                                validate={validateWord}
+                                onChange={formik.handleChange}
+                                type="Word"
+                                placeholder="Enter word"
+                            />
+                            <button type="submit" disabled={!formik.isValid || !formik.dirty}>Info!</button>
+                        </Form>
+                        );
+                    }}
+                    </Formik>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
