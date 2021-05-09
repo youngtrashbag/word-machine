5634bcf79f34c94ee46a4a4567e86ffd966d6e1d
youngtrashbag <yuule.smeker@gmail.com>
2021-05-09 10:19:09 +0200

    fix: database works now

--- src/database.rs
+++ src/database.rs
@@ -1,51 +1,63 @@
 use crate::word::Word;
 
 // TODO: maybe use different sled::Tree for each language
+// TODO: always opening and closing db connection in not good code, but it has to do for now
+//          also, the application is not intended for big data traffic
+
+fn db_path() -> String {
+    std::env::var("DB_PATH").expect("DB_PATH in environment file not set")
+}
 
 impl Word {
     /// fetch a word from database
-    pub fn get(word: String) -> Result<Word, sled::Error> {
-        let db = sled::Db;
+    pub fn get(word: String) -> Result<Self, sled::Error> {
+        let db = sled::open(db_path()).unwrap();
 
         match db.get(word.as_bytes())? {
             None => {
                 warn!("{}", format!("Could not find Word \"{}\"", word));
+                drop(db);
 
-                Err(sled::Error::ReportableBug(format!format!("Could not find Word \"{}\"", word)))
+                Err(sled::Error::ReportableBug(format!("Could not find Word \"{}\"", word)))
             },
             Some(w) => {
-                Ok(Word::deserialize(w.to_vec()))
+                drop(db);
+                Ok(Word::byte_deserialize(w.to_vec()))
             }
         }
     }
 
     /// insert or update word in db
-    pub fn update(word: Word) -> Result<Word, sled::Error> {
-        let db = sled::Db;
+    pub fn update(word: Word) -> Result<Self, sled::Error> {
+        let db = sled::open(db_path()).unwrap();
 
-        match db.insert(word.word.as_bytes())? {
+        match db.insert(word.word.as_bytes(), word.byte_serialize())? {
             None => {
-                warn!("{}", format!("Could not insert/update Word \"{}\"", word));
+                warn!("{}", format!("Could not insert/update Word \"{}\"", word.word));
+                drop(db);
 
-                Err(sled::Error::ReportableBug(format!format!("Could not insert/update Word \"{}\"", word)))
+                Err(sled::Error::ReportableBug(format!("Could not insert/update Word \"{}\"", word.word)))
             },
             Some(w) => {
-                Ok(Word::deserialize(w.to_vec()))
+                drop(db);
+                Ok(Word::byte_deserialize(w.to_vec()))
             }
         }
     }
 
-    pub fn delete(word: String) -> Result<Word, sled::Error> {
-        let db = sled::Db;
+    pub fn delete(word: String) -> Result<Self, sled::Error> {
+        let db = sled::open(db_path()).unwrap();
 
         match db.remove(word.as_bytes())? {
             None => {
                 warn!("{}", format!("Could not delete Word \"{}\"", word));
+                drop(db);
 
                 Err(sled::Error::ReportableBug(format!("Could not delete Word \"{}\"", word)))
             },
             Some(w) => {
-                Ok(Word::deserialize(w.to_vec()))
+                drop(db);
+                Ok(Word::byte_deserialize(w.to_vec()))
             }
         }
     }
