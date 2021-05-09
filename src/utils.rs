1c961b0001ed100c70c5862e7fc1bfab0d23a4e4
youngtrashbag <yuule.smeker@gmail.com>
2021-05-09 10:16:51 +0200

    chore: add utils with message struct
    - prepend byte_ to word (de)serialize functions, in order to avoid confusion

---
+++ src/utils.rs
@@ -0,0 +1,31 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize,Deserialize)]
+pub struct Message {
+    message: String,
+}
+
+impl Message {
+    /// new message, where parameter should be typed as a static string
+    /// ## Example:
+    /// ```
+    /// Message::new("Hello World");
+    /// ```
+    pub fn new(message: &str) -> Self {
+        Message {
+            message: message.to_string(),
+        }
+    }
+
+    /// Quick Conversion to JSON
+    /// consumes self
+    /// ## Example:
+    /// ```
+    /// Message::new("Hello World").to_json();
+    /// // output:
+    /// // {"message": "Hello World"}
+    /// ```
+    pub fn to_json(self) -> String {
+        serde_json::to_string(&self).unwrap()
+    }
+}
--- src/word.rs
+++ src/word.rs
@@ -3,7 +3,7 @@ use serde::{Serialize, Deserialize};
 use crate::language::Language;
 
 #[derive(Serialize,Deserialize)]
-struct Word {
+pub struct Word {
     /// the word itself
     pub word: String,
     /// list of synonyms (maybe synonyms will have None as this field)
@@ -16,12 +16,12 @@ struct Word {
 
 impl Word {
     /// serialize into byte vector, in order to save object in database
-    pub fn serialize(&self) -> Vec<u8> {
+    pub fn byte_serialize(&self) -> Vec<u8> {
         bincode::serialize(&self).unwrap()
     }
 
     /// deserialize from byte vector, so object can be read from database
-    pub fn deserialize(json: Vec<u8>) -> Self {
-        bincode::deserialize(&json).unwrap()
+    pub fn byte_deserialize(bytes: Vec<u8>) -> Self {
+        bincode::deserialize(&bytes).unwrap()
     }
 }
