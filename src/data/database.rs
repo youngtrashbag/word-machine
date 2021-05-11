12af8c0dd501f88e46cac0325e5d5606a9a3c8ce
youngtrashbag <yuule.smeker@gmail.com>
2021-05-11 16:05:17 +0200

    chore: remove comments
    and add newline to parse.rs

--- src/data/database.rs
+++ src/data/database.rs
@@ -1,9 +1,5 @@
 use lib::word::Word;
 
-// TODO: maybe use different sled::Tree for each language
-// TODO: always opening and closing db connection in not good code, but it has to do for now
-//          also, the application is not intended for big data traffic
-
 pub fn db_path() -> String {
     std::env::var("DB_PATH").expect("DB_PATH in environment file not set")
 }
