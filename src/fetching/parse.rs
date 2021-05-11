54dc119ceaa3e85936726320f51c9374d36074e2
youngtrashbag <yuule.smeker@gmail.com>
2021-05-11 14:28:41 +0200

    fix: no more pesky double quotes
    when parsing

--- src/fetching/parse.rs
+++ src/fetching/parse.rs
@@ -7,10 +7,12 @@ use lib::language::Language;
 pub fn parse(json: String) -> Word {
     let v: Value = serde_json::from_str(&json).unwrap();
 
+    info!("word {}", v[0]["meta"]["id"].as_str().unwrap());
+
     Word {
-        word: v[0]["meta"]["id"].to_string(),
+        word: v[0]["meta"]["id"].as_str().unwrap().to_string(),
         synonyms: Some(serde_json::from_value::<Vec<String>>(v[0]["meta"]["syns"][0].clone()).unwrap()),
-        definition: v[0]["shortdef"][0].to_string(),
+        definition: v[0]["shortdef"][0].as_str().unwrap().to_string(),
         language: Language::English,
     }
 }
