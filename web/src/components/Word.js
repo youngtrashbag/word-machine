693ecad65dc1f3d151932529179e38a36c8886b7
youngtrashbag <yuule.smeker@gmail.com>
2021-05-17 08:20:05 +0200

    feat: synonyms are clickable too
    <Link> element from react-router-dom did not work
    had to use <a> instead
    
    also package-lock lockfileVersion is 2 now (?)

--- web/src/components/Word.js
+++ web/src/components/Word.js
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 import "../Word.scss";
 
 const Word = (word) => {
-    const link = `/word/${word.word}`
     return (
         <>
         <div className="WordContainer">
-            <h1><Link to={ link }>{ word.word }</Link></h1>
+            <h1><Link to={ `/word/${word.word}` }>{ word.word }</Link></h1>
             <p>{ word.definition }</p>
             <div className="SynonymContainer">
                 <ul>
                 {
-                    word.synonyms.map( s => <li>{ s }</li>)
+                    word.synonyms.map( s => <li><a href={ `/word/${s}` }>{ s }</a></li>)
                 }
                 </ul>
             </div>
