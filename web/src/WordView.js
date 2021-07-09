842ef8f24bc6a406b73c3bc66f01f42b54198c35
youngtrashbag <yuule.smeker@gmail.com>
2021-07-09 13:28:45 +0200

    feat: port numbers are now env vars
    add react-scripts dependency

--- web/src/WordView.js
+++ web/src/WordView.js
@@ -13,7 +13,7 @@ const WordView = (props) => {
 
     useEffect((word) => {
         async function fetchWord(word) {
-            const response = await fetch(`http://localhost:4322/word/${language}/${word}`, {
+            const response = await fetch(`http://localhost:${process.env.REACT_APP_PORT_DATA}/word/${language}/${word}`, {
                 method: "GET", // *GET, POST, PUT, DELETE, etc.
                 mode: "cors", // no-cors, *cors, same-origin
                 cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -22,7 +22,7 @@ const WordView = (props) => {
                 referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
             });
 
-            return await response;
+            return await response.clone();
         }
 
         setResponse(fetchWord(word));
