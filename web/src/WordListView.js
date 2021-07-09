4760c8af90f9e14cc0de7732b0eaa96c6b2fd370
youngtrashbag <yuule.smeker@gmail.com>
2021-07-09 14:03:25 +0200

    fix: promises/async works now again !!

--- web/src/WordListView.js
+++ web/src/WordListView.js
@@ -8,52 +8,51 @@ const WordListView = (props) => {
     // setting as false because it serves double purpose as "hasLoaded"
     const [ response, setResponse ] = useState(false);
 
-    useEffect(() => {
-        async function fetchWords() {
-            const response = await fetch(`http://localhost:${process.env.REACT_APP_PORT_DATA}/all_words/${language}`, {
-                method: "GET", // *GET, POST, PUT, DELETE, etc.
-                mode: "cors", // no-cors, *cors, same-origin
-                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: "same-origin", // include, *same-origin, omit
-                redirect: "follow", // manual, *follow, error
-                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            });
+    useEffect(async () => {
+        const response = await fetch(`http://localhost:${process.env.REACT_APP_PORT_DATA}/all_words/${language}`, {
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        });
 
-            return await response.clone();
-        }
-
-        setResponse(fetchWords());
-    }, []);
+        setResponse({
+            ok: await response.ok,
+            json: await response.json(),
+        });
+    }, [language]);
 
     if (!response) {
         return (<h1>Loading...</h1>);
-    }
-
-    if (response.ok) {
-        return (
-            <>
-                <ul>
-                {
-                response.json().map( function(w) {
-                    return (
-                        <>
-                        <Word word={ w } />
-                        <br/>
-                        </>
-                    )
-                })
-                }
-                </ul>
-            </>
-        );
     } else {
-        return (
-            <>
-            <div className="Error">
-                <p>Could not fetch words</p>
-            </div>
-            </>
-        );
+        if (response.ok) {
+            return (
+                <>
+                    <ul>
+                    {
+                    response.json.map( function(w) {
+                        return (
+                            <>
+                            <Word word={ w } />
+                            <br/>
+                            </>
+                        )
+                    })
+                    }
+                    </ul>
+                </>
+            );
+        } else {
+            return (
+                <>
+                <div className="Error">
+                    <p>Could not fetch words</p>
+                </div>
+                </>
+            );
+        }
     }
 }
 
--- web/src/WordView.js
+++ web/src/WordView.js
@@ -11,43 +11,43 @@ const WordView = (props) => {
     // setting as false because it serves double purpose as "hasLoaded"
     const [ response, setResponse ] = useState(false);
 
-    useEffect((word) => {
-        async function fetchWord(word) {
-            const response = await fetch(`http://localhost:${process.env.REACT_APP_PORT_DATA}/word/${language}/${word}`, {
-                method: "GET", // *GET, POST, PUT, DELETE, etc.
-                mode: "cors", // no-cors, *cors, same-origin
-                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: "same-origin", // include, *same-origin, omit
-                redirect: "follow", // manual, *follow, error
-                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            });
+    // eslint-disable-next-line
+    useEffect(async () => {
+        const response = await fetch(`http://localhost:${process.env.REACT_APP_PORT_DATA}/word/${language}/${word}`, {
+            method: "GET", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        });
 
-            return await response.clone();
-        }
-
-        setResponse(fetchWord(word));
-    }, [word]);
+        setResponse({
+            ok: await response.ok,
+            json: await response.json(),
+        });
+    }, [word, language]);
 
     if (!response) {
         return (<h1>Loading...</h1>);
-    }
-
-    if (response.ok) {
-        return (
-            <>
-            <div className="Wrapper">
-                <Word word={ response.json() } />
-            </div>
-            </>
-        );
     } else {
-        return (
-            <>
-            <div className="Error">
-                <p>Could not fetch word {word}</p>
-            </div>
-            </>
-        );
+        if (response.ok) {
+            return (
+                <>
+                <div className="Wrapper">
+                    <Word word={ response.json } />
+                </div>
+                </>
+            );
+        } else {
+            return (
+                <>
+                <div className="Error">
+                    <p>Could not fetch word {word}</p>
+                </div>
+                </>
+            );
+        }
     }
 }
 
